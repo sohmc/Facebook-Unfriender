fe13a42fda53f5c7eb60cde6e09abd26890500e9
Michael Soh <github.sdkq@zulu.mikesoh.com>
2016-05-12 09:15:35 -0400

    Initial file

---
+++ facebook_unfriend.user.js
@@ -0,0 +1,27 @@
+﻿// ==UserScript==
+// @name            Facebook Bulk Unfriend
+// @namespace       
+// @description     Allows for bulk unfriends on Facebook
+// @version         0.1
+// @license         GPL 3.0
+// @include         http*://*.facebook.com/*/friends*
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
+// ==/UserScript==
+
+$jq = this.jQuery = jQuery.noConflict(true);
+
+$jq( document ).ready(function() {
+    alert('Ready.');
+    find_profileids();
+});
+
+
+function find_profileids() {
+    $jq('a[data-profileid]').after(function(index) {
+        console.log(index + ': ' + $jq(this).attr('data-profileid'));
+        return "<br/><p>foo</p>";
+    });
+
+    
+}
+
